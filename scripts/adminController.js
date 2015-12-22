915692dcdb09941fa87371e01795ac73db4abb4f
carovasquez7 <carolina.vasquez@usach.cl>
2015-12-22 13:51:18 -0300

    Se arreglan problemas de vista del admin

--- scripts/adminController.js
+++ scripts/adminController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var myApp = angular.module('myApp');
 
 myApp.controller("verEncuestas", function($scope,$http){
@@ -181,34 +181,4 @@ myApp.controller('agregarEncuesta', ['$scope', function($scope) {
   $scope.reset();
 }]);
 
-=======
-myApp.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/404");
-  $stateProvider
-    .state('adminalumnos', {
-      url: "/adminalumnos",
-      templateUrl: "views/admin/admin-alumnos.html",
-      authenticate: true
-    })
-    .state('adminprofesores', {
-      url: "/adminprofesores",
-      templateUrl: "views/admin/admin-profesores.html",
-      authenticate: true
-    })
-    .state('adminencuestas', {
-      url: "/adminencuestas",
-      templateUrl: "views/admin/admin-encuestas.html",
-      authenticate: true
-    })
-    .state('admintencuestas', {
-      url: "/admintencuestas",
-      templateUrl: "views/admin/admin-tencuestas.html",
-      authenticate: true
-    })
-    .state('admintencuestasnueva', {
-      url: "/admintencuestasnueva",
-      templateUrl: "views/admin/admin-te-nueva.html",
-      authenticate: true
-    })
-});
->>>>>>> master
+
