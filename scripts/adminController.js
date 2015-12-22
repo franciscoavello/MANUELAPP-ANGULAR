4d3ca86af44704c7e7683a8535c6803b25f2c063
Sergio Medina <medina159@gmail.com>
2015-12-22 20:51:09 -0300

    Agregados las consultas GET de la api a admin - falta ver el tema de "todas las encuestas"

--- scripts/adminController.js
+++ scripts/adminController.js
@@ -1,110 +1,40 @@
 
 var myApp = angular.module('myApp');
 
-myApp.controller("verEncuestas", function($scope,$http){
-      $scope.encuestas = [
-        {
-          id: 1,
-          nombre: "Encuesta PINGESO",
-          tipo: "Encuesta 360",
-          curso: "Proyecto de Ingeniería de Software",
-          fecha: "25/11/2015",
-          estado: "finalizada",
-          grupo: "1",
-          jefe: "Juan Peréz"
-        },
-        { 
-          id: 2,
-          nombre: "Encuesta IHC",
-          tipo: "Encuesta 360",
-          curso: "Interfaz humano-computador",
-          fecha: "15/11/2015",
-          estado: "pendiente",
-          grupo: "3",
-          jefe: "Francisco Riquelme"
-        },
-        {
-          id: 3,
-          nombre: "Encuesta IHC",
-          tipo: "Encuesta Liderazgo",
-          curso: "Interfaz humano-computador",
-          fecha: "18/11/2015",
-          estado: "pendiente",
-          grupo: "3",
-          jefe: "Francisco Riquelme"
-        },
-        {
-          id: 4,
-          nombre: "Encuesta PBD",
-          tipo: "Encuesta 360",
-          curso: "Proyecto de Base de Datos",
-          fecha: "01/11/2015",
-          estado: "finalizada",
-          grupo: "4",
-          jefe: "Pedro Paredes"
-        }
-
-      ];
-      $scope.cursos = [];
-
-
+myApp.controller("verEncuestas", function($scope,$http,$rootScope){
+      $http.get("http://localhost:3000/mostrar_encuestas")
+        .success(function(data){
+          $scope.encuestas = data;          
+        })
+        .error(function(err){
+        });
 });
 
+myApp.controller("verTiposEncuestas", function($scope,$http,$rootScope){
+      $http.get("http://localhost:3000/mostrar_tipos_encuestas")
+        .success(function(data){
+          $scope.tipos = data;          
+        })
+        .error(function(err){
+        });
+});
 
 myApp.controller("verProfesores", function($scope,$http){
-        $scope.profesores = [
-        {
-          id: 1,
-          nombre: "Jorge Salas",
-          curso: "Algoritmos Avanzados",
-          
-        },
-        { 
-          id: 2,
-          nombre: "Pedro Guzman",
-          curso: "Calculo Avanzado",
-        },
-        {
-          id: 3,
-          nombre: "Carlos Antillanca",
-          curso: "Topicos de Matematica",
-        },
-        {
-          id: 4,
-          nombre: "Eduardo Gamboa",
-          curso: "Futbol Avanzado",
-          
-        }
-      ];
+ $http.get("http://localhost:3000/buscar_por_rol?rol=1")
+    .success(function(data) {
+      $scope.profesores = data;
+    });
 
 });
 
 
 
-myApp.controller("verAlumnos", function($scope,$http){
-        $scope.alumnos = [
-        {
-          id: 1,
-          nombre: "Sergio Carmona",
-          
-        },
-        { 
-          id: 2,
-          nombre: "Manuel Guzman",
-          
-        },
-        {
-          id: 3,
-          nombre: "Carlos Salas",
-          
-        },
-        {
-          id: 4,
-          nombre: "Claudio Bravo",
-          
-        }
-      ];
-
+myApp.controller('verAlumnos', function ($http, $scope, $state) {
+  $http.get("http://localhost:3000/buscar_por_rol?rol=2")
+    .success(function(data) {
+      $scope.alumnos = data;
+    });
+  
 });
 
 
